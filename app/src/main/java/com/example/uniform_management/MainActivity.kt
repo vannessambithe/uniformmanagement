6442d2fa1e8043b2efff4cd0824bc4a67ef1ca5b
clencyc <clency2023@gmail.com>
2023-05-08 11:51:57 +0300

    uniform

---
+++ app/src/main/java/com/example/uniform_management/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.uniform_management
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+git add README.md
+git commit -m "uniform"
+git branch -M main
+git remote add origin https://github.com/vannessambithe/uniformmanagement.git
+git push -u origin main
